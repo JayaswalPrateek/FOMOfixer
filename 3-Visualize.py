555ced6bbeead54e64964f02f05e4839471e0ddc
Prateek Jayaswal <jayaswalprateek@gmail.com>
2024-05-23 23:39:38 +0530

    feat: add support for plotting suggest.json

---
+++ 3-Visualize.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import importlib
+
+
+def plotFreqDistr(listOfDicts):
+    frequencies = {key: value for key, value in listOfDicts.items() if key is not None}
+    counts = [list(frequencies.values()).count(i) for i in range(2, 26)]
+    plt.bar(range(2, 26), counts, color="skyblue")
+    plt.xlabel("Frequency")
+    plt.ylabel("Count")
+    plt.title("Frequency Distribution")
+    plt.show()
+
+
+if __name__ == "__main__":
+    plotFreqDistr(importlib.import_module("2-Suggest").deserialize("suggest"))
+    print("SUCCESS")
