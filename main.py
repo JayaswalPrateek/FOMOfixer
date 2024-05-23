44af5c15deb5c1a2f319e125c7f8d672ece622ba
Prateek Jayaswal <jayaswalprateek@gmail.com>
2024-05-23 14:07:12 +0530

    feat: add scaffolding for seeding pipeline
    added sqlite3 dependency for seeding pipeline which is responsible for building seed's dict and storing it into a sqlite3 db after preprocessing it.
    
    Signed-off-by: Prateek Jayaswal <jayaswalprateek@gmail.com>

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from print_dict import print_dict as prettyPrintMyDict
 from tweeterpy import TweeterPy
 from tweeterpy import config
 import argparse
+import sqlite3
+
+DEBUG = True
 
 
 def setup(username, password):
+    if DEBUG:
+        return
     twitter = TweeterPy()
     print("PSA: Avoid using your primary account")
     twitter.login(username, password)
@@ -13,6 +18,23 @@ def setup(username, password):
         exit(1)
     else:
         prettyPrintMyDict(twitter.me)
+
+
+def getSeedDB(seedUsername):
+    return seedDictToDB(preProcessSeedDict(buildSeedDict(seedUsername)))
+
+
+def buildSeedDict(seedUsername):
+    pass
+
+
+def preProcessSeedDict(uglySeedDict):
+    pass
+
+
+def seedDictToDB(cleanSeedDict):
+    conn = sqlite3.connect("db/seed.db")
+    return conn
 
 
 if __name__ == "__main__":
@@ -24,4 +46,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     setup(args.username, args.password)
 
+    conn = getSeedDB(input("Enter Seed Username: "))
+
+    conn.close()
     print("SUCCESS")
